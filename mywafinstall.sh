d7c0a06106cff29ddae8d4002105b2ab5109b0b2
Yoan Agostini <yoan.agostini@gmail.com>
2013-05-23 17:06:16 +0200

    Fix

--- mywafinstall.sh
+++ mywafinstall.sh
@@ -14,45 +14,53 @@
 # If not, see http://www.gnu.org/licenses/.
 
 function installMyWaf {
-        echo "[*] Installation du WAF :"
-        # packages
-        echo "[*] - Installation des packages d'administration"
-		apt-get update && apt-get -y purge exim4-base exim4-config exim4-daemon-light && apt-get install -y tcpdump portmap nfs-server && apt-get install -y libfile-tail-perl && apt-get -y install python-dev python-pip && pip install glances
-        # optimisations
-        echo "[*] - Optimisations systeme"
-        echo "* - nofile 65536" > /etc/security/limits.conf
-        # Nginx + Naxsi
-        echo "[*] - Installation de nginx & naxsi"
-        echo "deb http://ftp.debian.org/debian/ wheezy-backports main" >> /etc/apt/sources.list
-        apt-get update  
-        apt-get -y --force-yes -t wheezy-backports install nginx-naxsi
-		echo "[*] - Installation de MyWaf"
-		if [ ! -d /usr/local/mywaf ]; then
-			mkdir -p /usr/local/mywaf
-		fi
-		apwd=`pwd`
-		cd /usr/local/mywaf
-		wget https://raw.github.com/aNaoy/MyWaf/master/mywaf.sh
-		wget https://raw.github.com/aNaoy/MyWaf/master/sysctl.conf
-		wget https://raw.github.com/aNaoy/MyWaf/master/vhost.tpl
-		wget https://raw.github.com/aNaoy/MyWaf/master/README.md
-		chmod +x mywaf.sh
-		ln -s mywaf.sh /usr/local/bin/
-		cd apwd
-		echo "[*] - Configuration et optimisation"
-		# Optimisation de nginx
-        echo 'ULIMIT="-n 65536"' >> /etc/default/nginx
-        ## Netfilter / FW / iptables
-        apt-get -y install iptables module-assistant xtables-addons-common;
-        module-assistant --verbose --text-mode auto-install xtables-addons;
-        # No DNS
-        update-rc.d bind9 remove
-        # Hostname
-        hostname MyWaf
-        echo "127.0.0.1 mywaf" >> /etc/hosts
-        hostname > /etc/hostname
-        ## Ajout de la whitelist WAF
-        echo "127.0.0.1" > /usr/local/etc/waf_whitelist.txt
+	echo \n\n\n\n"[************************]"
+    echo "[*] Installation du WAF :"
+	echo "[************************]"
+    # packages
+	echo \n\n\n\n"[***********************************************]"
+    echo "[*] - Installation des packages d'administration"
+	echo "[***********************************************]"
+	apt-get update && apt-get -y purge exim4-base exim4-config exim4-daemon-light && apt-get install -y tcpdump portmap nfs-server && apt-get install -y libfile-tail-perl && apt-get -y install python-dev python-pip && pip install glances
+    # optimisations
+	echo \n\n\n\n"[**************************]"
+    echo "[*] - Optimisations systeme"
+	echo "[**************************]"
+    echo "* - nofile 65536" > /etc/security/limits.conf
+    # Nginx + Naxsi
+	echo \n\n\n\n"[**********************************]"
+    echo "[*] - Installation de nginx & naxsi"
+	echo "[**********************************]"
+    echo "deb http://ftp.debian.org/debian/ wheezy-backports main" >> /etc/apt/sources.list
+    apt-get update  
+    apt-get -y --force-yes -t wheezy-backports install nginx-naxsi
+	echo "[*] - Installation de MyWaf"
+	if [ ! -d /usr/local/mywaf ]; then
+		mkdir -p /usr/local/mywaf
+	fi
+	apwd=`pwd`
+	cd /usr/local/mywaf
+	wget https://raw.github.com/aNaoy/MyWaf/master/mywaf.sh
+	wget https://raw.github.com/aNaoy/MyWaf/master/sysctl.conf
+	wget https://raw.github.com/aNaoy/MyWaf/master/vhost.tpl
+	wget https://raw.github.com/aNaoy/MyWaf/master/README.md
+	chmod +x mywaf.sh
+	cp mywaf.sh /usr/local/bin/
+	cd apwd
+	echo "[*] - Configuration et optimisation"
+	# Optimisation de nginx
+    echo 'ULIMIT="-n 65536"' >> /etc/default/nginx
+    ## Netfilter / FW / iptables
+    apt-get -y install iptables module-assistant xtables-addons-common;
+    module-assistant --verbose --text-mode auto-install xtables-addons;
+    # No DNS
+    update-rc.d bind9 remove
+    # Hostname
+    hostname MyWaf
+    echo "127.0.0.1 mywaf" >> /etc/hosts
+    hostname > /etc/hostname
+    ## Ajout de la whitelist WAF
+    echo "127.0.0.1" > /usr/local/etc/waf_whitelist.txt
 }
 
 if [[ $EUID -ne 0 ]]; then
