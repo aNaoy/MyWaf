7a270984b2227d09cf600507febe1a7ad284c525
Yoan Agostini <yoan.agostini@gmail.com>
2013-05-23 17:23:04 +0200

    Fix

--- mywafinstall.sh
+++ mywafinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # (c) Yoan AGOSTINI <yoan {DOT IS HERE} agostini {AT IS HERE} gmail {DOT IS HERE} com>
-# (c) Ronan LEBON
+# (c) Ronan LEBON <ronan {DOT IS HERE} lebon {AT IS HERE} gmail {DOT IS HERE} com>
 # (c) Ingesup
 
 # MyWaf is free software: you can redistribute it and/or modify it under the terms of the
@@ -14,27 +14,27 @@
 # If not, see http://www.gnu.org/licenses/.
 
 function installMyWaf {
-	echo \n\n\n\n"[************************]"
+	echo "[************************]"
     echo "[*] Installation du WAF :"
 	echo "[************************]"
     # packages
-	echo \n\n\n\n"[***********************************************]"
+	echo "[***********************************************]"
     echo "[*] - Installation des packages d'administration"
 	echo "[***********************************************]"
 	apt-get update && apt-get -y purge exim4-base exim4-config exim4-daemon-light && apt-get install -y tcpdump portmap && apt-get -y install python-dev python-pip && pip install glances
     # optimisations
-	echo \n\n\n\n"[**************************]"
+	echo "[**************************]"
     echo "[*] - Optimisations systeme"
 	echo "[**************************]"
     echo "* - nofile 65536" > /etc/security/limits.conf
     # Nginx + Naxsi
-	echo \n\n\n\n"[**********************************]"
+	echo "[**********************************]"
     echo "[*] - Installation de nginx & naxsi"
 	echo "[**********************************]"
     echo "deb http://ftp.debian.org/debian/ wheezy-backports main" >> /etc/apt/sources.list
     apt-get update  
     apt-get -y --force-yes -t wheezy-backports install nginx-naxsi
-	echo \n\n\n\n"[**************************]"
+	echo "[**************************]"
 	echo "[*] - Installation de MyWaf"
 	echo "[**************************]"
 	if [ ! -d /usr/local/mywaf ]; then
@@ -49,14 +49,11 @@ function installMyWaf {
 	chmod +x mywaf.sh
 	cp mywaf.sh /usr/local/bin/
 	cd apwd
-	echo \n\n\n\n"[**********************************]"
+	echo "[**********************************]"
 	echo "[*] - Configuration et optimisation"
 	echo "[**********************************]"
 	# Optimisation de nginx
     echo 'ULIMIT="-n 65536"' >> /etc/default/nginx
-    ## Netfilter / FW / iptables
-    apt-get -y install iptables module-assistant xtables-addons-common;
-    module-assistant --verbose --text-mode auto-install xtables-addons;
     # No DNS
     update-rc.d bind9 remove
     # Hostname
